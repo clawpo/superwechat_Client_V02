34c54b8f9b6ac197a3d5e8ef89aed5a5a0babcfe
耿小泰 <clawpo@foxmail.com>
2016-05-17 11:01:02 +0800

    添加修改群组名称的非空和重名判断

--- src/cn/ucai/superwechat/activity/EditActivity.java
+++ src/cn/ucai/superwechat/activity/EditActivity.java
@@ -1,16 +1,17 @@
 package cn.ucai.superwechat.activity;
 
-import cn.ucai.superwechat.R;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import cn.ucai.superwechat.R;
+
 public class EditActivity extends BaseActivity{
 	private EditText editText;
 
+    private String oldGroupName;
 
 	@Override
 	protected void onCreate(Bundle arg0) {
@@ -22,15 +23,24 @@ public class EditActivity extends BaseActivity{
 		String data = getIntent().getStringExtra("data");
 		if(title != null)
 			((TextView)findViewById(R.id.tv_title)).setText(title);
-		if(data != null)
-			editText.setText(data);
+		if(data != null) {
+            editText.setText(data);
+            oldGroupName = data;
+        }
 		editText.setSelection(editText.length());
 		
 	}
 	
 	
 	public void save(View view){
-		setResult(RESULT_OK,new Intent().putExtra("data", editText.getText().toString()));
-		finish();
+        String groupName = editText.getText().toString();
+		if(groupName.isEmpty()||groupName.trim().isEmpty()){
+            startActivity(new Intent(this, AlertDialog.class).putExtra("msg", "群组名称不能为空"));
+        }else if(oldGroupName.equals(groupName.trim())){
+            startActivity(new Intent(this, AlertDialog.class).putExtra("msg", "群组名称没有修改"));
+        }else {
+            setResult(RESULT_OK, new Intent().putExtra("data", editText.getText().toString()));
+            finish();
+        }
 	}
 }
